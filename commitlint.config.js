ebef70b8c5c16a68758ad51fda496824d1849657
y1j2x34 <y1j2x34@qq.com>
2021-03-15 12:51:33 +0800

    chore(commitlint): upgrade commitlint version

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,27 +1,4 @@
 module.exports = {
-    rules: {
-        'body-leading-blank': [1, 'always'],
-        'footer-leading-blank': [1, 'always'],
-        'header-max-length': [2, 'always', 72],
-        'scope-case': [2, 'always', 'lowerCase'],
-        'subject-empty': [2, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
-        'type-case': [2, 'always', 'lowerCase'],
-        'type-empty': [2, 'never'],
-        'type-enum': [
-            2,
-            'always', [
-                'build',
-                'chore',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test'
-            ]
-        ]
-    }
+    extends: ["@commitlint/config-conventional"],
+    formatter: "@commitlint/format"
 };
