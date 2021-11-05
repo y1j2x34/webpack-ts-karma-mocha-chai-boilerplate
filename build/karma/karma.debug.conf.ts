bfcef841c56282f1c853527c63a5f7ea32181681
y1j2x34 <y1j2x34@qq.com>
2021-11-05 14:54:04 +0800

    chore: add karma-htmlfile-reporter

--- build/karma/karma.debug.conf.ts
+++ build/karma/karma.debug.conf.ts
@@ -1,8 +1,11 @@
 import karma from 'karma';
 import baseConfig from './karma.base.conf';
 import webpackTest from '../webpack/webpack.test';
-
+interface HTMLFileReporterConfigOption extends karma.ConfigOptions {
+    htmlFileReporter?: Record<string, unknown>;
+};
 export default function(config: karma.Config) {
+    baseConfig.plugins?.push('karma-htmlfile-reporter');
     config.set(baseConfig as karma.ConfigOptions);
     config.set({
         preprocessors: {
@@ -12,7 +15,13 @@ export default function(config: karma.Config) {
         webpackMiddleware: {
             stats: 'errors-warnings'
         },
-        reporters: ['progress', 'mocha'],
-        logLevel: config.LOG_DEBUG
-    });
+        reporters: ['progress', 'mocha', 'html'],
+        logLevel: config.LOG_DEBUG,
+        htmlReporter: {
+            outputFile: '../../reports/index.html',
+            groupSuites: true,
+            useCompactStyle: true,
+            useLegacyStyle: true
+        }
+    } as HTMLFileReporterConfigOption);
 }
